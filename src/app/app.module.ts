22dda6ea4fdc68262b8dbbf8846b1cf98c42fd3d
JonyDMorales <jonatan_moralest@hotmail.com>
2019-09-28 16:23:23 -0500

    Se configura nav y un poco del header del home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { APP_ROUTING } from  './app.routing';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
+    APP_ROUTING,
     BrowserModule
   ],
   providers: [],
